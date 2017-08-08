2f47f05a7bbeb693d20cc637ca6c90346c1aa678
Erik Brinkman <erik.brinkman@gmail.com>
2017-08-08 00:47:17 -0400

    Bump version and ga version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setuptools
 
 setuptools.setup(
     name='egta',
-    version='0.0.4',
+    version='0.0.5',
     description='Scripts to perform EGTA',
     url='https://github.com/egtaonline/quiesce.git',
     author='Strategic Reasoning Group',
@@ -11,7 +11,7 @@ setuptools.setup(
     license='Apache 2.0',
     entry_points=dict(console_scripts=['egta=egta.egta:main']),
     install_requires=[
-        'gameanalysis~=3.1',
+        'gameanalysis~=3.2',
         'egtaonlineapi~=0.1',
         'numpy~=1.13',
     ],
