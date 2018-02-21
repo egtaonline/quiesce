73094ccb6a4dfd646a7a5700777277c252d21d48
Erik Brinkman <erik.brinkman@gmail.com>
2018-02-21 10:16:34 -0500

    Fix docs

--- sphinx/conf.py
+++ sphinx/conf.py
@@ -49,7 +49,7 @@ for mod_name in MOCK_MODULES:
 sys.path.insert(0, path.abspath('..'))
 
 # Run api-doc automatically
-apidoc.main(['<prog-name>', '-efo', '.', path.join('..', 'egta')])
+apidoc.main(['-efo', '.', path.join('..', 'egta')])
 
 # -- General configuration ------------------------------------------------
 
